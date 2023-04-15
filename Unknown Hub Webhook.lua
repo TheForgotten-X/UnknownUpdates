29f803e29f74da4269b8879b677514312795fbcc
TheForgotten-X <106926639+TheForgotten-X@users.noreply.github.com>
2023-04-15 16:00:17 -0400

    Update Unknown Hub Webhook.lua

--- Unknown Hub Webhook.lua
+++ Unknown Hub Webhook.lua
@@ -1,2 +1,4 @@
+--[[I Know you can see these and by the way these automaticlly update so deleting them dont do much but we should talk im curious as to your motive's 
+Ping me in my server saying [ Im here ] ill be waiting]]
 local Webhook = "https://discord.com/api/webhooks/1096488339760222238/l2sNFvp81aylfjjiIo3_7N0IW5rZqo_5_9tpQEbUz9vF6-6Hkfoqd4BuuYU2_GBgs045m"
 return Webhook
